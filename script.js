7476016d99f26e8a65c79b3aebdc4dfffe0cfe4e
kzbdz <104859288+kzbdz@users.noreply.github.com>
2022-05-04 09:52:32 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// cm  to meters
+function calc(data){
+    let box  =data;
+   console.log("cm to meter:     ",data/100);
+   
+  
+}
+calc(100)
+calc(200)
+
+
+//meter to cm
+function calcm(datam){
+ let boxcm  =datam*100;
+ console.log("meter to cm:    ",datam*100)
+ }
+
+calcm(1)
+calcm(20)
